17bee89fd28ec2298a1ac555d5cecd9d1be0f25e
Yui Jensen <kyanyui67@gmail.com>
2024-07-08 16:18:43 +0200

    add hammer img

---
+++ script.js
@@ -0,0 +1,12 @@
+const cursor = document.querySelector(".cursor");
+
+window.addEventListener("mousemove", (e) => {
+  cursor.style.top = e.pageY + "px";
+  cursor.style.left = e.pageX + "px";
+});
+window.addEventListener("mousedown", () => {
+  cursor.classList.add("active");
+});
+window.addEventListener("mouseup", () => {
+  cursor.classList.remove("active");
+});
