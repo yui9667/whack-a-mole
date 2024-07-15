ca69e059d8c01ac751d71ec86f56353488c9d74d
Yui Jensen <kyanyui67@gmail.com>
2024-07-15 16:44:37 +0200

    Now hammer works

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ window.addEventListener("mousedown", () => {
 window.addEventListener("mouseup", () => {
   cursor.classList.remove("active");
 });
+
+window.onload = function () {
+  setGame();
+};
+
+function setGame() {
+  for (let i = 0; i < 9; i++) {
+    let title = document.createElement("div");
+    title.id = i.toString();
+    document.getElementById("board").appendChild(title);
+  }
+}
